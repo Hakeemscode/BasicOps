d74f4a77386ae3385f0b3dac22d387c0131d176b
atandaabdulhakeem862@gmail.com <atandaminat@gmail.com>
2024-02-06 12:33:29 -0800

    Initial commit

---
+++ src/Basicops.java
@@ -0,0 +1,15 @@
+public class Basicops {
+    public static void main(String[] args) {
+
+        /*
+        for loops
+        while loops
+         */
+        int i = 0;
+
+        while (i < 5) {
+            System.out.println("counting.. " + i);
+            i++;
+        }
+        }
+    }
